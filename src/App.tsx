473cc97f855b4326b562891c05d45682455ec9fe
oleksandrgrebinskij <roki000sasha@gmail.com>
2024-08-29 17:31:15 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View} from 'react-native';
+import {Root} from 'src/modules/root';
+
+function App(): React.JSX.Element {
+  return (
+    <View style={{flex: 1}}>
+      <Root />
+    </View>
+  );
+}
+export default App;
---
+++ src/modules/root/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export const Root = () => {
+  return (
+    <View style={{flex: 1}}>
+      {/*<GestureHandlerRootView style={styles.container}>*/}
+      {/*  <NavigationContainer>*/}
+      {/*  </NavigationContainer>*/}
+      {/*</GestureHandlerRootView>*/}
+      <Text>asdasdasd</Text>
+    </View>
+  );
+};
